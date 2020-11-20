b2fe603fb01e2ffc27ea316e54e361e3e3fec3f0
Jonny Bergdahl <jonny@bergdahl.it>
2020-11-20 22:45:02 +0100

    Work in progress

--- code.py
+++ code.py
@@ -26,24 +26,7 @@ print('Pressure: {}hPa'.format(bme680_sensor.pressure))
 sgp30_sensor = adafruit_sgp30.Adafruit_SGP30(i2c)
 print("eCO2 = %d ppm \t TVOC = %d ppb" % (sgp30_sensor.eCO2, sgp30_sensor.TVOC))
 
-displayio.release_displays()
-display_bus = displayio.FourWire(board.SPI(), command=board.EPD_DC,
-                                 chip_select=board.EPD_CS,
-                                 reset=board.EPD_RESET, baudrate=1000000)
-
-
-time.sleep(1)
-display = adafruit_il0373.IL0373(
-    display_bus,
-    width=296,
-    height=128,
-    rotation=270,
-    black_bits_inverted=False,
-    color_bits_inverted=False,
-    grayscale=True,
-    refresh_time=1,
-    seconds_per_frame=1
-)
+display = board.DISPLAY
 
 led = DigitalInOut(board.D13)
 led.direction = Direction.OUTPUT
@@ -52,32 +35,28 @@ led.value = True
 splash = displayio.Group(max_size=20)
 # background
 rect = Rect(0, 0, 296, 128, fill=0xFFFFFF)
-
 # Text
-font = bitmap_font.load_font("/Helvetica-Bold-16.bdf")
+font = bitmap_font.load_font("/Exo-Bold-75.bdf")
 color = 0x000000
-text = "Temperature {:4.2f} C\nHumidity {:4.2f}%\nPressure {:6.2f}\n".format(
-            bme680_sensor.temperature, 
-            bme680_sensor.humidity,
-            bme680_sensor.pressure)
+text = "{:4.2f}°".format(bme680_sensor.temperature)
 text_area1 = label.Label(font, text=text, color=color)
-text_area1.x = 40
-text_area1.y = 20
+text_area1.x = 28
+text_area1.y = 40
 text_area1.background_color=0xFFFFFF
-
 ## Bitmaps
 thermometer_bitmap = displayio.OnDiskBitmap(open("/thermometer.bmp", "rb"))
 tilegrid = displayio.TileGrid(thermometer_bitmap, pixel_shader=displayio.ColorConverter())
 tilegrid.x = 10
 tilegrid.y = 10
 
-#splash.append(rect)
+splash.append(rect)
 splash.append(text_area1)
 splash.append(tilegrid)
 
 display.show(splash)
+print("WTF")
 display.refresh()
-
+print("WTF")
 elapsed_sec = 0
 
 while True:
@@ -94,9 +73,6 @@ while True:
         print("SGP TVOC = %d ppb" % (sgp30_sensor.TVOC))
 
         elapsed_sec = 0
-        text_area1.text = "Temperature {:4.2f} C\nHumidity {:4.2f}%\nPressure {:6.2f}\n".format(
-            bme680_sensor.temperature, 
-            bme680_sensor.humidity,
-            bme680_sensor.pressure)
+        text_area1.text = "{:4.2f}°".format(bme680_sensor.temperature)
         display.show(splash)
         display.refresh()
