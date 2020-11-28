ade0ef3dae1f60eb9eff7787a5a0837f2491145f
Jonny Bergdahl <jonny@bergdahl.it>
2020-11-28 23:50:40 +0100

    Changed font generation

--- tools/makefonts.cmd
+++ tools/makefonts.cmd
@@ -1,14 +1,19 @@
 SET DPI=112
 SET FONT=Exo-SemiBold
-SET SIZE=18
-otf2bdf3\otf2bdf -v -r %DPI% -p %SIZE% -o %FONT%-%SIZE%.bdf -l "45 46 48_57 77 98 109 112" %FONT%.ttf
+SET SIZE=6
+otf2bdf3\otf2bdf -v -r %DPI% -p %SIZE% -o %FONT%-%SIZE%.bdf -l "32_126 161_255" %FONT%.ttf
 move /y %FONT%-%SIZE%.bdf ..\src
 
-SET SIZE=36
-otf2bdf3\otf2bdf -v -r %DPI% -p %SIZE% -o %FONT%-%SIZE%.bdf -l "37 48_57 77 80 97 104" %FONT%.ttf
+SET FONT=Exo-SemiBold
+SET SIZE=12
+otf2bdf3\otf2bdf -v -r %DPI% -p %SIZE% -o %FONT%-%SIZE%.bdf -l "32_126 161_255" %FONT%.ttf
+move /y %FONT%-%SIZE%.bdf ..\src
+
+SET SIZE=18
+otf2bdf3\otf2bdf -v -r %DPI% -p %SIZE% -o %FONT%-%SIZE%.bdf -l "32_126 161_255" %FONT%.ttf
 move /y %FONT%-%SIZE%.bdf ..\src
 
 SET FONT=Exo-Bold
-SET SIZE=72
-otf2bdf3\otf2bdf -v -r %DPI% -p 72 -o %FONT%-%SIZE%.bdf -l "46 48_57 77 186" %FONT%.ttf
+SET SIZE=42
+otf2bdf3\otf2bdf -v -r %DPI% -p %SIZE% -o %FONT%-%SIZE%.bdf -l "32_126 161_255" %FONT%.ttf
 move /y %FONT%-%SIZE%.bdf ..\src
