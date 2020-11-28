545aa603d36b7cf80f85fe23f6a4a0353742c99f
Jonny Bergdahl <jonny@bergdahl.it>
2020-11-28 20:10:37 +0100

    Change fonts

--- tools/makefonts.cmd
+++ tools/makefonts.cmd
@@ -1,7 +1,14 @@
-otf2bdf3\otf2bdf -v -p 18 -o ExoSemiBold-18.bdf -l "45 46 48_57 77 98 109 112" Exo-SemiBold.ttf
-otf2bdf3\otf2bdf -v -p 36 -o ExoSemiBold-36.bdf -l "37 48_57 77 80 97 104" Exo-SemiBold.ttf
-otf2bdf3\otf2bdf -v -p 72 -o ExoBold-72.bdf -l "46 48_57 77 186" Exo-Bold.ttf
+SET DPI=112
+SET FONT=Exo-SemiBold
+SET SIZE=18
+otf2bdf3\otf2bdf -v -r %DPI% -p %SIZE% -o %FONT%-%SIZE%.bdf -l "45 46 48_57 77 98 109 112" %FONT%.ttf
+move /y %FONT%-%SIZE%.bdf ..\src
 
-move /y ExoSemiBold-18.bdf ..\src
-move /y ExoSemiBold-36.bdf ..\src
-move /y ExoBold-72.bdf ..\src
+SET SIZE=36
+otf2bdf3\otf2bdf -v -r %DPI% -p %SIZE% -o %FONT%-%SIZE%.bdf -l "37 48_57 77 80 97 104" %FONT%.ttf
+move /y %FONT%-%SIZE%.bdf ..\src
+
+SET FONT=Exo-Bold
+SET SIZE=72
+otf2bdf3\otf2bdf -v -r %DPI% -p 72 -o %FONT%-%SIZE%.bdf -l "46 48_57 77 186" %FONT%.ttf
+move /y %FONT%-%SIZE%.bdf ..\src
