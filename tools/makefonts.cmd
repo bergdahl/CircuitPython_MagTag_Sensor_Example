562d1daa74ab1b5fd85ae27600456272068d1dd7
Jonny Bergdahl <jonny@bergdahl.it>
2020-11-29 01:52:22 +0100

    Changed tiny font

--- tools/makefonts.cmd
+++ tools/makefonts.cmd
@@ -1,6 +1,6 @@
 SET DPI=112
 SET FONT=Exo-SemiBold
-SET SIZE=6
+SET SIZE=9
 otf2bdf3\otf2bdf -v -r %DPI% -p %SIZE% -o %FONT%-%SIZE%.bdf -l "32_126 161_255" %FONT%.ttf
 move /y %FONT%-%SIZE%.bdf ..\src
 
